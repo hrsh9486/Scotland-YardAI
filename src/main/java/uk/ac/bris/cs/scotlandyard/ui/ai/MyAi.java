da3ee7f535e6762bb144d6f5f96731dff5185971
Harish <hrsh9486@gmail.com>
2025-03-05 15:24:29 +0000

    Implemented Floyd Warshall to get distances between all node pairs

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,21 +1,114 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.Random;
+import java.lang.reflect.AnnotatedArrayType;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.graph.ImmutableValueGraph;
 import io.atlassian.fugue.Pair;
-import uk.ac.bris.cs.scotlandyard.model.Ai;
-import uk.ac.bris.cs.scotlandyard.model.Board;
-import uk.ac.bris.cs.scotlandyard.model.Move;
+import nonapi.io.github.classgraph.json.JSONUtils;
+import uk.ac.bris.cs.scotlandyard.model.*;
+import uk.ac.bris.cs.scotlandyard.ui.model.Setup;
+
 
 public class MyAi implements Ai {
+	ArrayList<ArrayList<Integer>> distances;
 
-	@Nonnull @Override public String name() { return "Name me!"; }
+
+	// Returns a matrix storing the shortest path from every node to every other node, using
+	// Floyd Warshall Algorithm
+	public ArrayList<ArrayList<Integer>> makeDistancesFW(Board board) {
+		ImmutableValueGraph graph = board.getSetup().graph;
+		ArrayList<ArrayList<Integer>> distances = new ArrayList<>();
+
+		// Sets initial values as infinity or 0
+		System.out.println(graph.nodes().size());
+		for (Object node1: graph.nodes()) {
+			distances.add(new ArrayList<>());
+			for (Object node2 : graph.nodes()) {
+				Integer node1Num = (Integer) node1;
+				Integer node2Num = (Integer) node2;
+				if (node1 == node2) {
+					distances.get(node1Num - 1).add(node2Num - 1, 0);
+				}
+				else if (graph.hasEdgeConnecting(node1, node2)) {
+					distances.get(node1Num - 1).add(node2Num - 1, 1);
+				}
+				else {
+					distances.get(node1Num - 1 ).add(node2Num - 1, 9999);
+				}
+			}
+		}
+
+		for( Integer k= 0; k<distances.size(); k++ ){
+			for( Integer i= 0; i<distances.size(); i++ ){
+				for( Integer j= 0; j<distances.size(); j++ ){
+					if(distances.get(i).get(j) > distances.get(i).get(k) + distances.get(k).get(j) ){
+						distances.get(i).set(j,(distances.get(i).get(k) + distances.get(k).get(j)));
+					}
+				}
+			}
+		}
+		return distances;
+	}
+
+	@Nonnull @Override public String name() { return "Doofenshmirtz"; }
+
+
+	public Integer score(Board board) {
+		// Get moves mrX can make (destination)
+		// Get distance from all the destinations to the available moves of the detective
+		// Add a score to each destination based on the above
+		// Pick the score which is optimal (highest or lowest, depending on implementation)
+		return 0;
+	}
+
+
+	public Board.GameState constructGameState (Board board){
+		PseudoGameStateFactory factory = new PseudoGameStateFactory();
+		ImmutableSet<Piece> pieces = board.getPlayers();
+		ImmutableSet<Move> moves = board.getAvailableMoves();
+		ArrayList<Player> players = new ArrayList<>();
+
+		for (Piece piece : pieces) {
+			if (piece.isDetective()){
+				// For types of tickets, call .getCount() and add to an immutable map
+				HashMap<ScotlandYard.Ticket, Integer> playerTicketCount = new HashMap<>();
+				for (ScotlandYard.Ticket ticket : ScotlandYard.DETECTIVE_TICKETS ) {
+					playerTicketCount.put(ticket, board.getPlayerTickets(piece).get().getCount(ticket));;
+				}
+
+				Integer location  = board.getDetectiveLocation((Piece.Detective) piece).orElse(-1);
+				players.add(new Player(piece, ImmutableMap.copyOf(playerTicketCount), location));
+			}
+			else if (piece.isMrX()) {
+				HashMap<ScotlandYard.Ticket, Integer> mrXTicketCount = new HashMap<>();
+				for (ScotlandYard.Ticket ticket : ScotlandYard.MRX_TICKETS ) {
+					mrXTicketCount.put(ticket, board.getPlayerTickets(piece).get().getCount(ticket));;
+				}
+				Integer location  = board.getAvailableMoves().asList().get(0).source();
+				Player mrX = new Player(piece,ImmutableMap.copyOf(mrXTicketCount), location );
+				players.add(0, mrX);
+
+			}
+		}
+
+		Player mrX = players.get(0);
+		List detectives = players.subList(1,players.size());
+		return factory.build(board.getSetup(), mrX, ImmutableList.copyOf(detectives));
+	}
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
-		// returns a random move, replace with your own implementation
+
+
+		Board.GameState myGameState = constructGameState(board);
 		var moves = board.getAvailableMoves().asList();
 		return moves.get(new Random().nextInt(moves.size()));
 	}
