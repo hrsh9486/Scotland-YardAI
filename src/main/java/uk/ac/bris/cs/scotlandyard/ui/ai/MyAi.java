b5e886965956ebdc0cf0df0f52af88c28f0fd521
Harish <hrsh9486@gmail.com>
2025-03-05 16:56:02 +0000

    Implemented score. Begun minimax, but broken

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -10,19 +10,25 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.graph.ImmutableValueGraph;
+import com.google.common.graph.ValueGraph;
 import io.atlassian.fugue.Pair;
 import nonapi.io.github.classgraph.json.JSONUtils;
 import uk.ac.bris.cs.scotlandyard.model.*;
 import uk.ac.bris.cs.scotlandyard.ui.model.Setup;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 
 public class MyAi implements Ai {
 	ArrayList<ArrayList<Integer>> distances;
+	ArrayList<Player> detectives;
+	Player mrX;
 
 
 	// Returns a matrix storing the shortest path from every node to every other node, using
 	// Floyd Warshall Algorithm
-	public ArrayList<ArrayList<Integer>> makeDistancesFW(Board board) {
+	public void  makeDistancesFW(Board board) {
 		ImmutableValueGraph graph = board.getSetup().graph;
 		ArrayList<ArrayList<Integer>> distances = new ArrayList<>();
 
@@ -54,26 +60,62 @@ public class MyAi implements Ai {
 				}
 			}
 		}
-		return distances;
+		this.distances =  distances;
 	}
 
 	@Nonnull @Override public String name() { return "Doofenshmirtz"; }
 
 
-	public Integer score(Board board) {
-		// Get moves mrX can make (destination)
-		// Get distance from all the destinations to the available moves of the detective
-		// Add a score to each destination based on the above
-		// Pick the score which is optimal (highest or lowest, depending on implementation)
-		return 0;
+	//gametree function
+	// We need to take our base state, add a node for every possible move, repeat for every player
+
+
+	public Pair<Move, Integer> minimax(Board.GameState gs, Move move, Integer depth, boolean mrXTurn ){
+		if (depth == 0){
+			//return  move which minimizes score(gs,mrX.location());
+			return new Pair<> (move, score(gs,move.source()));
+		}
+		if (mrXTurn){
+			Integer maxEval = -9999;
+			Pair<Move, Integer> bestEval = new Pair<>(move,maxEval);
+			for (Move m: gs.getAvailableMoves()){
+				Pair<Move, Integer> eval = minimax(gs.advance(m), m, depth-1, false);
+				if (eval.right() > bestEval.right()) {
+					bestEval = eval;
+				}
+			}
+			return bestEval;
+		}
+
+		else {
+			Integer minEval = 9999;
+			Pair<Move, Integer> bestEval = new Pair<>(move,  minEval);
+			for (Move m: gs.getAvailableMoves()){
+				Pair<Move, Integer> eval = minimax(gs.advance(m),m , depth-1, true);
+				if (eval.right()< bestEval.right()){
+					bestEval = eval;
+				}
+			}
+			return bestEval;
+		}
+
 	}
 
 
-	public Board.GameState constructGameState (Board board){
-		PseudoGameStateFactory factory = new PseudoGameStateFactory();
+
+
+
+	public Integer score(Board.GameState gameState, Integer destination) {
+		Integer maxDistance = 0;
+		for (Player p : detectives) {
+			maxDistance = max(maxDistance, distances.get(destination).get(p.location()));
+		}
+		return maxDistance;
+	}
+
+	public void createPlayers(Board board) {
 		ImmutableSet<Piece> pieces = board.getPlayers();
-		ImmutableSet<Move> moves = board.getAvailableMoves();
-		ArrayList<Player> players = new ArrayList<>();
+		ArrayList<Player> detectives = new ArrayList<>();
 
 		for (Piece piece : pieces) {
 			if (piece.isDetective()){
@@ -84,7 +126,7 @@ public class MyAi implements Ai {
 				}
 
 				Integer location  = board.getDetectiveLocation((Piece.Detective) piece).orElse(-1);
-				players.add(new Player(piece, ImmutableMap.copyOf(playerTicketCount), location));
+				detectives.add(new Player(piece, ImmutableMap.copyOf(playerTicketCount), location));
 			}
 			else if (piece.isMrX()) {
 				HashMap<ScotlandYard.Ticket, Integer> mrXTicketCount = new HashMap<>();
@@ -93,13 +135,20 @@ public class MyAi implements Ai {
 				}
 				Integer location  = board.getAvailableMoves().asList().get(0).source();
 				Player mrX = new Player(piece,ImmutableMap.copyOf(mrXTicketCount), location );
-				players.add(0, mrX);
+				this.mrX = mrX;
 
 			}
 		}
+		this.detectives = detectives;
+	}
 
-		Player mrX = players.get(0);
-		List detectives = players.subList(1,players.size());
+	public Board.GameState constructGameState (Board board){
+		PseudoGameStateFactory factory = new PseudoGameStateFactory();
+		ImmutableSet<Move> moves = board.getAvailableMoves();
+
+		createPlayers(board);
+		Player mrX = this.mrX;
+		List detectives = this.detectives;
 		return factory.build(board.getSetup(), mrX, ImmutableList.copyOf(detectives));
 	}
 
@@ -109,6 +158,10 @@ public class MyAi implements Ai {
 
 
 		Board.GameState myGameState = constructGameState(board);
+
+		return minimax(Board.GameState, )
+
+
 		var moves = board.getAvailableMoves().asList();
 		return moves.get(new Random().nextInt(moves.size()));
 	}
